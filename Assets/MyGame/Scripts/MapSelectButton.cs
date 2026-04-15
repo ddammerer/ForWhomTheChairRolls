48c0e918fb9b07d387203dd08420a507ff65761d
Fabian <Fabian.Grasboeck07@htl-salzburg.ac.at>
2026-04-15 09:55:47 +0200

    add VR Menue

--- Assets/MyGame/Scripts/MapSelectButton.cs
+++ Assets/MyGame/Scripts/MapSelectButton.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
-public class MapSelectButton : MonoBehaviour {
-    // badmap id = 1
-    // goodmap id = 2
-    public void OnClick(int id) {
-        SceneManager.LoadScene(id);
+public class DropdownButton : MonoBehaviour
+{
+  public Dropdown dropdown;
+
+
+ 
+    public void MapSelect()
+    {
+        string selectedValue = dropdown.options[dropdown.value].text;
+
+
+        switch (selectedValue)
+        {
+            case "Good Map":
+                SceneManager.LoadScene(2);
+                break;
+
+            case "Bad Map":
+                SceneManager.LoadScene(1);
+
+                break;
+
+            default:
+                Debug.LogWarning("Unbekannte Option: " + selectedValue);
+                break;
+        }
     }
+
+
 }
